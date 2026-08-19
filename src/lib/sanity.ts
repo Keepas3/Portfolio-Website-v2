03b96390384e1039f190c80cb3108a601187f868
Keepas3 <bfun679@gmail.com>
2026-08-19 16:08:32 -0400

    Fix

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -1,8 +1,13 @@
 import { createClient } from '@sanity/client';
 
 export const sanity = createClient({
-  projectId: import.meta.env.SANITY_PROJECT_ID,
-  dataset: import.meta.env.SANITY_DATASET ?? 'production',
+  projectId: 
+    import.meta.env.PUBLIC_SANITY_PROJECT_ID || 
+    import.meta.env.SANITY_PROJECT_ID,
+  dataset: 
+    import.meta.env.PUBLIC_SANITY_DATASET || 
+    import.meta.env.SANITY_DATASET || 
+    'production',
   apiVersion: '2024-01-01',
   useCdn: true,
 });
